25c212c697ccb073d377293f061f09e894f6c45b
Ygor Victor Santos <ygor.vs.code@gmail.com>
2025-02-16 17:20:20 -0300

    calcular baskara

--- meus-exercicios/javascript/equação/script.js
+++ meus-exercicios/javascript/equação/script.js
@@ -1,7 +1,4 @@
-// delta = b²-4.a.c
-// x = -b +- raiz de delta /2.a
-
-var a = Number(5)
+/* var a = Number(5)
 var b = Number(3)
 var c = Number(2)
 
@@ -15,5 +12,26 @@ else{
     var xo = Number(- b - (delt**0.5)) / (2*a)
 
     console.log(`S= {${xa}, ${xo}}`)
+} */
+
+
+
+function calcular()
+{
+    var va = window.document.querySelector('#a')
+    var vb = window.document.querySelector('#b')
+    var vc = window.document.querySelector('#c')
+    var res = window.document.querySelector('#res')
+    var delt = Number(vb.value**2-4*va.value*vc.value)
+    var x1 = Number(- vb.value + (delt**0.5)) / (2*va.value)
+    var x2 = Number(- vb.value - (delt**0.5)) / (2*va.value)
+
+if (delt < 0)
+    {
+        res.innerHTML = 'O delta é menor que 0'
+} else{
+    res.innerHTML = `S= {${x1}, ${x2}}`
 }
 
+
+}
