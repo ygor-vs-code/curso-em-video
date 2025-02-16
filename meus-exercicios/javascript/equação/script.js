8889a6ee78fec200bea6652a42e92986df9844fe
Ygor Victor Santos <ygor.vs.code@gmail.com>
2025-02-16 18:33:17 -0300

    equação
    corrigindo problemas para mobile

--- meus-exercicios/javascript/equação/script.js
+++ meus-exercicios/javascript/equação/script.js
@@ -1,20 +1,4 @@
-/* var a = Number(5)
-var b = Number(3)
-var c = Number(2)
-
-var delt = Number(b**2-4*a*c)
-
-if (delt < 0){
-    console.log('Não')
-}
-else{
-    var xa = Number(- b + (delt**0.5)) / (2*a)
-    var xo = Number(- b - (delt**0.5)) / (2*a)
-
-    console.log(`S= {${xa}, ${xo}}`)
-} */
-
-
+//script
 
 function calcular() {
     var va = window.document.querySelector('#a')
