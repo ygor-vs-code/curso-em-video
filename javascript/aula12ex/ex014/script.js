cfa22cb95ad72c17978b4224c4bc123bcfb9cd76
Ygor Victor Santos <ygor.vs.code@gmail.com>
2025-02-22 19:30:32 -0300

    ex14

---
+++ javascript/aula12ex/ex014/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('manha')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        img.src = 'assets/manha.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE!
+        img.src = 'assets/tarde.jpg'
+        document.body.style.background = '#b9846f'
+    } else {
+        // BOA NOITE!
+        img.src = 'assets/noite.jpg'
+        document.body.style.background = '#515154'
+    }
+}
