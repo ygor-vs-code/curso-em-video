a2365f6bc532276b2da24070b1b1c5f4abf23ee7
Ygor Victor Santos <ygor.vs.code@gmail.com>
2025-03-08 16:46:27 -0300

    ex016

---
+++ javascript/aula14ex/ex016/script.js
@@ -0,0 +1,12 @@
+let i = document.querySelector('#inicio')
+let f = document.querySelector('#fim')
+let p = document.querySelector('#passo')
+let res = document.querySelector('#res')
+
+function calcular() {
+    let inicio = Number(i.value)
+    let fim = Number(f.value)
+    let passo = Number(p.value)
+
+    console.log(inicio)
+}
